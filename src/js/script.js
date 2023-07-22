17952cdd9dc47e7ee7b4bd57946f615cb82f5aa8
Deivisson Lisboa <dvilaflor@gmail.com>
2023-07-22 12:49:59 -0300

    Create resize button
    Add resize, toggle border and clean button to settings div

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,12 @@
 const GRID = document.querySelector(".grid")
+const SIZE_BTN = document.getElementById("size")
+const BORDER_BTN = document.getElementById("toggle-border")
+const SAVE_BTN = document.getElementById("save")
+const CLEAN_BTN = document.getElementById("clean")
 
 function setGrid(width = 16) {
   GRID.innerHTML = ""
+  GRID.setAttribute("style", `--grid-width: ${width};`)
 
   for (let i = 0; i < width * width; i++) {
     cell = document.createElement("div")
@@ -10,13 +15,29 @@ function setGrid(width = 16) {
     GRID.append(cell)
   }
 
-  return document.querySelectorAll(".grid .cell")
+  let cells = document.querySelectorAll(".grid .cell")
+
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.setAttribute("style", "background-color: #000;")
+    })
+  })
 }
 
-let cells = setGrid()
+setGrid()
 
-cells.forEach((cell) => {
-  cell.addEventListener("mouseover", () => {
-    cell.setAttribute("style", "background-color: #000;")
-  })
+SIZE_BTN.addEventListener("click", () => {
+  let newSize = parseInt(prompt("Width should be between 16 and 100*"))
+
+  if (newSize < 16 || newSize > 100) {
+    newSize = parseInt(
+      prompt("Width should be between 16 and 100*\nTHERE IS NO OTHER OPTION!")
+    )
+  }
+
+  setGrid(newSize)
+})
+
+BORDER_BTN.addEventListener("click", () => {
+  GRID.classList.toggle("border")
 })
