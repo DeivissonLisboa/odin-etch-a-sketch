50c004657f0f7bd4dce54ca080872e2f25ba0919
Deivisson Lisboa <dvilaflor@gmail.com>
2023-07-22 15:37:23 -0300

    Add extra credits functions
    Create rainbow and gradient tools

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,54 @@
 const GRID = document.querySelector(".grid")
-const SIZE_BTN = document.getElementById("size")
+const RESIZE_BTN = document.getElementById("size")
 const BORDER_BTN = document.getElementById("toggle-border")
+const RAINBOW_BTN = document.getElementById("rainbow")
+const GRADIENT_BTN = document.getElementById("gradient")
 const CLEAN_BTN = document.getElementById("clean")
 const SAVE_BTN = document.getElementById("save")
 const DOWNLOAD_ANCHOR = document.getElementById("download-link")
+const COLORS = [
+  "#f94144",
+  "#f3722c",
+  "#f8961e",
+  "#f9c74f",
+  "#90be6d",
+  "#43aa8b",
+  "#577590",
+]
+let rainbow = false
+let gradient = false
+let currentColorIndex = 0
 let currentWidth = 16
+
+function addCellEventListiner(cell) {
+  cell.addEventListener("mouseover", () => {
+    if (rainbow) {
+      cell.setAttribute(
+        "style",
+        `background-color: ${COLORS[currentColorIndex]};`
+      )
+      currentColorIndex = (currentColorIndex + 1) % COLORS.length
+    } else if (gradient) {
+      if (cell.dataset.gradient) {
+        let color = cell.dataset.gradient
+
+        if (color <= 0) {
+          color = "000"
+        }
+
+        cell.setAttribute("style", `background-color: #${color};`)
+        cell.dataset.gradient = cell.dataset.gradient - 111
+      } else {
+        let color = 999
+        cell.dataset.gradient = color
+        cell.setAttribute("style", `background-color: #${color};`)
+      }
+    } else {
+      cell.setAttribute("style", "background-color: #000;")
+      cell.dataset.gradient = "000"
+    }
+  })
+}
 
 function setGrid(width) {
   GRID.innerHTML = ""
@@ -19,16 +63,10 @@ function setGrid(width) {
 
   let cells = document.querySelectorAll(".grid .cell")
 
-  cells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-      cell.setAttribute("style", "background-color: #000;")
-    })
-  })
+  cells.forEach((cell) => addCellEventListiner(cell))
 }
 
-setGrid(currentWidth)
-
-SIZE_BTN.addEventListener("click", () => {
+RESIZE_BTN.addEventListener("click", () => {
   let newSize = parseInt(prompt("Width should be between 16 and 100*"))
 
   if (newSize < 16 || newSize > 100) {
@@ -43,6 +81,16 @@ SIZE_BTN.addEventListener("click", () => {
 
 BORDER_BTN.addEventListener("click", () => {
   GRID.classList.toggle("border")
+})
+
+RAINBOW_BTN.addEventListener("click", () => {
+  rainbow = !rainbow
+  gradient = false
+})
+
+GRADIENT_BTN.addEventListener("click", () => {
+  gradient = !gradient
+  rainbow = false
 })
 
 CLEAN_BTN.addEventListener("click", () => {
@@ -62,3 +110,5 @@ SAVE_BTN.addEventListener("click", () => {
     }, 3 * 10 ** 4)
   })
 })
+
+let cells = setGrid(currentWidth)
